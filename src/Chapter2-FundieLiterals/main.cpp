007cc7cc98759b1e754a6f0c079b2a536130f678
Heinrich-Curtis <heinrich.curtis.bz@gmail.com>
2024-10-09 16:17:02 -0400

    Chapter 2 stuff

--- src/Chapter2-FundieLiterals/main.cpp
+++ src/Chapter2-FundieLiterals/main.cpp
@@ -5,6 +5,21 @@ using std::cout; using std::endl;
 
 int main()
 {
-	cout << "test2" << endl;
+	//integral types
+	int a{1};
+	int b{-1};
+	//int c {-1u}; //what does this do? Ohh, we get a narrowing conversion. -1 u is evaluated as 2^32 -1, then we get the narrowing conversion becasue its out of range of int
+
+	int d{1u};
+	long e{123u};
+
+	//octal numbers
+	int f{017}; //decimal 15? Yep , decimal 15 by default, have to specify the output format if we want otherwise
+	cout << "Ocatal with supplied literal 017: " << f << std::endl;
+	long long g{10'000UL};
+	cout << "unsigned long with apostrophe separator: " << g <<std::endl;
+	int h{0x1A};
+	int i{0b0110};
+	cout << "Hex 1A: " << h << ", binary 0110: " << i <<std::endl;
 	return 0;
 }
