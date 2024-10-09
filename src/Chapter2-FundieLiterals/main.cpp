c4bfba5b1d09839d0d86e922c9c23410b04897e1
Heinrich-Curtis <heinrich.curtis.bz@gmail.com>
2024-10-09 15:33:15 -0400

    build with cmake, sort out some dirs

---
+++ src/Chapter2-FundieLiterals/main.cpp
@@ -0,0 +1,10 @@
+//Instantiating literals of the fundamental data types using uniform initialization
+//import std; We can't use this yet becuase gcc suport isn't complete, this pre-compiled module isn't available yet
+#include <iostream>
+using std::cout; using std::endl;
+
+int main()
+{
+	cout << "test2" << endl;
+	return 0;
+}
